bbae53b5cd1b92f8486235b4002f2600cd68fe51
P4radise <emel.9616@gmail.com>
2022-08-31 16:10:20 +0500

    IHub-217335 Move hardcode data to parameters

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 Module retrieves files from SFTP, filters them by file name using parameters set in the settings file and uploads them to OneVizion.
 
 
-* sftpFileNameRegexpPattern* - a regexp pattern, which is used to select files from the SFTP
+* sftpFileNameRegexp* - a regexp pattern, which is used to select files from the SFTP
 * ovTrackorType* - target Trackor Type name
 * ovEfileFieldName* - target EFile field name
-* ovFieldMappings* - object. Parameters in it determine which Trackors need to be updated, using the received files. Object contains:
+* ovTrackorFilter* - object. Parameters in it determine which Trackors need to be updated, using the received files. Object contains:
   * searchConditions* - search string. More details can be found in the API documentation in the trackors section in Search Trackors. Either a constant or parameter names are used as values
   * searchConditionsParams - objects list. Object contains:
     * paramName - parameter name. It must be written instead of values in searchConditions
-    * regexpPattern - a regexp pattern
+    * paramValueRegexp - a regexp pattern
 
 
 Example of settings.json
@@ -29,29 +29,29 @@ Example of settings.json
 
     "sftpFileToOvMappings": [
         {
-            "sftpFileNameRegexpPattern": "^\\w+_\\w+_\\w+_\\d{8}_\\d{6}_([a-zA-Z]+\\d+|\\d+)\\.zip",
+            "sftpFileNameRegexp": "^\\w+_\\w+_\\w+_\\d{8}_\\d{6}_([a-zA-Z0-9]+)\\.zip",
             "ovTrackorType": "Project",
             "ovEfileFieldName": "P_MMUAT_FILE_SA",
-            "ovFieldMappings": {
-                "searchConditions": "equal(P_FUZE_PROJECT_ID_FZ,:value1) and equal(P_PROJECT_STATUS,Active)",
+            "ovTrackorFilter": {
+                "searchConditions": "equal(P_FUZE_PROJECT_ID_FZ,\\\":value1\\\") and equal(P_PROJECT_STATUS,Active)",
                 "searchConditionsParams": [
                     {
                         "paramName": "value1",
-                        "regexpPattern": "(.+)\\.zip$"
+                        "paramValueRegexp": "([a-zA-Z0-9]+)\\.zip$"
                     }
                 ]
             }
         },
         {
-            "sftpFileNameRegexpPattern": "^[a-zA-Z0-9]+\\-[A-Z]+\\-\\d+_LTE_\\w+_[a-zA-Z0-9]+_[0-9-]+_\\d{6}\\.zip",
+            "sftpFileNameRegexp": "^[a-zA-Z0-9]+\\-[A-Z]+\\-\\d+_LTE_\\w+_[a-zA-Z0-9]+_[0-9-]+_\\d{6}\\.zip",
             "ovTrackorType": "Project",
             "ovEfileFieldName": "P_LTE_ENV",
-            "ovFieldMappings": {
+            "ovTrackorFilter": {
                 "searchConditions": "equal(TRACKOR_KEY,:value1)",
                 "searchConditionsParams": [
                     {
                         "paramName": "value1",
-                        "regexpPattern": "^[a-zA-Z0-9]+\\-[A-Z]+\\-\\d+"
+                        "paramValueRegexp": "^[a-zA-Z0-9]+\\-[A-Z]+\\-\\d+"
                     }
                 ]
             }
--- module.py
+++ module.py
@@ -20,20 +20,18 @@ class Module:
             file_list = self._sftp_service.get_file_list(sftp)
             for sftp_file_to_ov_mapping in self._settings_data['sftpFileToOvMappings']:
                 trackor_type = sftp_file_to_ov_mapping['ovTrackorType']
-                regexp_pattern = sftp_file_to_ov_mapping['sftpFileNameRegexpPattern']
+                regexp_pattern = sftp_file_to_ov_mapping['sftpFileNameRegexp']
                 filtered_file_list = self._filter_files(file_list, regexp_pattern)
                 self._module_log.add(LogLevel.INFO, f'{len(filtered_file_list)} files found for the regexp pattern "{regexp_pattern}"')
 
                 for file_name in filtered_file_list:
-                    field_mappings = sftp_file_to_ov_mapping['ovFieldMappings']
-                    search_conditions = self._get_search_conditions(field_mappings, file_name)
-                    trackor_fields = self._get_trackor_fields(field_mappings['searchConditions'])
-                    trackor_data = self._trackor_service.get_trackors(trackor_type, trackor_fields, search_conditions)
-                    filtered_trackors = self._filter_trackors(trackor_fields, trackor_data, search_conditions)
-                    self._module_log.add(LogLevel.INFO, f'{len(filtered_trackors)} Trackors found for the file "{file_name}"')
+                    trackor_filter = sftp_file_to_ov_mapping['ovTrackorFilter']
+                    search_conditions = self._get_search_conditions(trackor_filter, file_name)
+                    trackor_data = self._trackor_service.get_trackors(trackor_type, search_conditions)
+                    self._module_log.add(LogLevel.INFO, f'{len(trackor_data)} Trackors found for the file "{file_name}"')
                     if len(trackor_data) > 0:
                         self._process_file_data(sftp, sftp_file_to_ov_mapping['ovEfileFieldName'], file_name,
-                            filtered_trackors, trackor_type)
+                            trackor_data, trackor_type)
 
         self._module_log.add(LogLevel.INFO, 'Module has been completed')
 
@@ -43,17 +41,17 @@ class Module:
 
         return filtered_files
 
-    def _get_search_conditions(self, field_mappings: dict, file_name: str) -> str:
-        search_conditions = field_mappings['searchConditions']
-        for conditions_params in field_mappings['searchConditionsParams']:
+    def _get_search_conditions(self, trackor_filter: dict, file_name: str) -> str:
+        search_conditions = trackor_filter['searchConditions']
+        for conditions_params in trackor_filter['searchConditionsParams']:
             value_from_file_name = None
             param_name = conditions_params['paramName'] if 'paramName' in conditions_params else None
-            regexp_pattern = conditions_params['regexpPattern'] if 'regexpPattern' in conditions_params else None
+            param_regexp_pattern = conditions_params['paramValueRegexp'] if 'paramValueRegexp' in conditions_params else None
 
-            value_from_file_name = self._get_value_from_file_name(file_name, regexp_pattern)
+            value_from_file_name = self._get_value_from_file_name(file_name, param_regexp_pattern)
             if value_from_file_name is None:
                 self._module_log(LogLevel.WARNING,
-                    f'Failed to get value for regexp pattern "{regexp_pattern}" ' \
+                    f'Failed to get value for regexp pattern "{param_regexp_pattern}" ' \
                     f'from file name "{file_name}" for trackor filter')
 
             search_conditions = search_conditions.replace(f':{param_name}', value_from_file_name)
@@ -68,41 +66,15 @@ class Module:
 
         return value_from_file_name
 
-    def _get_trackor_fields(self, search_conditions: str) -> str:
-        trackor_fields = None
-        for field in re.findall(Module.SEARCH_FIELD_PATTERN, search_conditions):
-            field_name = field[len('equal('):-1]
-            if trackor_fields is None:
-                trackor_fields = field_name
-            else:
-                trackor_fields = f'{field_name},{trackor_fields}'
-
-        return trackor_fields
-
-    def _filter_trackors(self, trackor_fields: str, trackor_data: list, trackor_filter: str) -> list:
-        filtered_trackors = []
-        fields_list = re.split(',', trackor_fields)
-        for trackor in trackor_data:
-            is_values_match = True
-            for field in fields_list:
-                if re.search(trackor[field], trackor_filter) is None:
-                    is_values_match = False
-                    break
-
-            if is_values_match:
-                filtered_trackors.append(trackor[TrackorService.TRACKOR_ID])
-
-        return filtered_trackors
-
-    def _process_file_data(self, sftp: Connection, field_name: str, file_name: str, filtered_trackors: list, trackor_type: str) -> None:
+    def _process_file_data(self, sftp: Connection, field_name: str, file_name: str, trackor_data: list, trackor_type: str) -> None:
         try:
             self._sftp_service.download_file(sftp, file_name)
             is_file_exists = os.path.exists(file_name)
             if is_file_exists:
                 self._module_log.add(LogLevel.DEBUG, f'File "{file_name}" has been downloaded')
 
-                for trackor in filtered_trackors:
-                    self._trackor_service.upload_file(trackor_type, trackor, field_name, file_name)
+                for trackor in trackor_data:
+                    self._trackor_service.upload_file(trackor_type, trackor[TrackorService.TRACKOR_ID], field_name, file_name)
 
                 self._sftp_service.move_to_archive(sftp, file_name)
                 self._module_log.add(LogLevel.INFO, f'File "{file_name}" has been uploaded and moved to the archive')
@@ -118,19 +90,20 @@ class Module:
 
 class TrackorService:
     TRACKOR_ID = 'TRACKOR_ID'
+    TRACKOR_KEY = 'TRACKOR_KEY'
 
     def __init__(self, ov_url: str, settings_data: dict) -> None:
         self._ov_url = ov_url
         self._ov_access_key = settings_data['ovAccessKey']
         self._ov_secret_key = settings_data['ovSecretKey']
 
-    def get_trackors(self, trackor_type: str, field_name: str, search_value: str) -> list:
+    def get_trackors(self, trackor_type: str, search_value: str) -> list:
         ov_trackor_type = Trackor(trackorType=trackor_type, URL=self._ov_url,
             userName=self._ov_access_key, password=self._ov_secret_key, isTokenAuth=True)
 
         ov_trackor_type.read(
             search=search_value,
-            fields={field_name}
+            fields={TrackorService.TRACKOR_KEY}
         )
 
         if len(ov_trackor_type.errors) == 0:
